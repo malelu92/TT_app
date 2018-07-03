b256201f4b1e70be1a2cc00bbe41ee5ab9827342
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.wv.cc.cmu.edu>
2018-07-03 17:39:12 -0400

    local storage working for one item

--- sheltr.js
+++ sheltr.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	sessionStorage.setItem('prep-card-1-count', 0);
+
 	$("#prep-card-1").click(function(){
 		var list = $("<div class='preparation-list-background'><div class='preparation-list'></div>");
     $("body").append(list);
@@ -30,22 +32,52 @@ $(document).ready(function() {
     								</div>");
     $(".preparation-list").append(content);
 
-    if(sessionStorage.getItem('item1') == "true") {
-	    console.log("lalala")
-	    /*$("#item1").setAttribute("checked","checked");*/
-	    document.getElementById("item1").setAttribute('checked','checked');
-	  }
+    console.log("---")
+    console.log(sessionStorage.getItem('item1'))
 
+    if(sessionStorage.getItem('item1') == "true") {
+    	console.log("**true")
+    	/*var count = sessionStorage.getItem('prep-card-1-count');
+    	sessionStorage.setItem('prep-card-1-count', count+1);*/
+	    document.getElementById("item1").checked = true;
+	  }
+	  else {
+	  	console.log("**false")
+	  	/*console.log("unchecked")
+	  	var count = sessionStorage.getItem('prep-card-1-count');
+    	sessionStorage.setItem('prep-card-1-count', count-1);*/
+	  	document.getElementById("item1").checked = false;
+	  }
 	});
 
 });
 
 
 function checkedItem1() {
-	console.log("lele")
-	sessionStorage.setItem('item1', true);
+	if(!(sessionStorage.getItem('item1'))){
+		sessionStorage.setItem('item1', true);
+		/*document.getElementById("item1").setAttribute('checked','checked');*/
+    var count = sessionStorage.getItem('prep-card-1-count');
+    sessionStorage.setItem('prep-card-1-count', count+1);
+	}
+	else {
+		if(sessionStorage.getItem('item1') == "true") {
+			sessionStorage.setItem('item1', false);
+			/*document.getElementById("item1").setAttribute('checked','not checked');*/
+			var count = sessionStorage.getItem('prep-card-1-count');
+			sessionStorage.setItem('prep-card-1-count', count-1);
+		}
+		else {
+			sessionStorage.setItem('item1', true);
+			/*document.getElementById("item1").setAttribute('checked','checked');*/
+    	var count = sessionStorage.getItem('prep-card-1-count');
+    	sessionStorage.setItem('prep-card-1-count', count+1);
+		}
+	}
+
 }
 
 function closePrepList() {
 	$(".preparation-list-background").remove();
+	document.getElementById("countdown-1").innerHTML = sessionStorage.getItem('prep-card-1-count') + " of 5";
 }
