06ba9d34c539c8626bc2688eb8fab11f63f75f24
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.wv.cc.cmu.edu>
2018-07-03 16:46:12 -0400

    session storage saving first item

--- sheltr.js
+++ sheltr.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$("#prep-card-1").click(function(){
 		var list = $("<div class='preparation-list-background'><div class='preparation-list'></div>");
     $("body").append(list);
@@ -6,7 +7,7 @@ $(document).ready(function() {
     									<div id='prep-list-1-close' onclick='closePrepList()' class='col-12'>close</div>\
     									<label class='container'>\
     										<div class='col-12'>Marina\
-    										<input type='checkbox'>\
+    										<input type='checkbox' id='item1' onclick='checkedItem1()'>\
     										<span class='checkmark'></span>\
     										</div>\
     										<div class='col-12'>Ketki\
@@ -28,9 +29,22 @@ $(document).ready(function() {
     									</label>\
     								</div>");
     $(".preparation-list").append(content);
+
+    if(sessionStorage.getItem('item1') == "true") {
+	    console.log("lalala")
+	    /*$("#item1").setAttribute("checked","checked");*/
+	    document.getElementById("item1").setAttribute('checked','checked');
+	  }
+
 	});
+
 });
 
+
+function checkedItem1() {
+	console.log("lele")
+	sessionStorage.setItem('item1', true);
+}
 
 function closePrepList() {
 	$(".preparation-list-background").remove();
